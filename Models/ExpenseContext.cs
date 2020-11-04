f02d774f79d872284215c4ff0db637934285ab78
Cata Pinzar <catalin.pinzar@gmail.com>
2020-11-04 19:23:27 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pinzar_Catalin_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+    }
+}
